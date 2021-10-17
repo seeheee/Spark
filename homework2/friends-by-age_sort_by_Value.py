2e328c6040cd09d856e43841882a58ef47ac17df
sehee <ksh2001ksh@naver.com>
2021-10-17 22:31:18 +0900

    2차과제 완료

---
+++ homework2/friends-by-age_sort_by_Value.py
@@ -0,0 +1,22 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("FriendsByAge")
+sc = SparkContext(conf = conf)
+
+def parseLine(line):
+    fields = line.split(',')
+    age = int(fields[2])
+    numFriends = int(fields[3])
+    return (age, numFriends)
+
+lines = sc.textFile("file:///SparkCourse/fakefriends.csv")
+rdd = lines.map(parseLine)
+totalsByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+averagesByAge = totalsByAge.mapValues(lambda x: x[0] / x[1])
+results = averagesByAge.collect()
+
+# 튜플을 추가로 정렬하는데 x[1]의 값으로 정렬함
+results_sort = sorted(results, key=lambda x: x[1])
+for result in results_sort:
+    print(result)
+
